fd1c92367b1eff581f542e19e7616bbffb077b9b
Franziska Kiel <kiel.franziska@outlook.com>
2025-01-14 15:32:34 +0100

    adds coding task 2: operators & data types

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 console.log("Hello World");
+
+//Use the concatenation operator to concatenate each letter of your firstname into a string one letter by one.
+console.log("F" + "r" + "a" + "n" + "z" + "i");
+
+// Error! Make it work
+// console.log("Hallo";
+console.log("Hallo");
+
+// Error! Make it work
+// console.log("That doesn"t work");
+console.log("That doesn't work");
+
+// should return true. Change only the operator to fix it.
+// console.log(50 + 2 > 52);
+console.log(50 + 2 >= 52);
+
+// should be false. Change only the operator to fix it.
+// console.log("333" == 333);
+console.log("333" === 333);
+
+// Wrong result: Expected 555 not 855. Change only the operator to fix it.
+// console.log(600 + 510 / 2);
+console.log((600 + 510) / 2);
+
+// Wrong result: Expected a Number 4 received 22
+// console.log("2" + 2);
+console.log(2 + 2);
+
+// Create a console.log that returns true when both division results are equal to 5
+// console.log(25 / 5); // Result: 5
+// console.log(50 / 10); // Result: 5
+console.log(25 / 5 === 5 && 50 / 10 === 5);
