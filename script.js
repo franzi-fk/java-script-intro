a8ff9ee1ad2aa29389423b8ff6f2e062d5672340
Franziska Kiel <kiel.franziska@outlook.com>
2025-01-15 10:56:06 +0100

    adds coding task 4

--- script.js
+++ script.js
@@ -55,10 +55,53 @@ function calcGroddPrice(net, tax) {
 calcGroddPrice(20, 0.19);
 
 // Implement an addPositive function
-// If any of the arguments is a negative number, it should be handled as a positive number
+// If any of the arguments is a negative number, it should be handled
+// as a positive number
 function addPositive(a, b) {
   if (a < 0) a = -a;
   if (b < 0) b = -b;
   console.log(a + b);
 }
 addPositive(-3, 4);
+
+// --------------------------------------------------------------
+// Coding Task 4
+// --------------------------------------------------------------
+// Copy the following complete code block to your script.js file
+// and try to spot and fix the errors.
+/* const userName = Brad;
+userName = "Jenna";
+function getUserNameLength {
+  return const result = name.length
+}
+console.log(getUserNameLength(userName) > 4)
+// ^______________ Should log true */
+let userName = "Brad";
+userName = "Jenna";
+
+function getUserNameLength(userName) {
+  return userName.length;
+}
+console.log(getUserNameLength(userName) > 4);
+// ^______________ Should log true
+
+// --------------------------------------------------------------
+// TODO: Implement the isString function
+let isString = function (anything) {
+  return typeof anything === "string";
+};
+
+console.log(isString("Hello"));
+// result should be true
+
+console.log(isString(3));
+// result should be false
+
+console.log(isString(undefined));
+// result should be false
+
+console.log(isString(""));
+// result should be true
+
+console.log(isString("John" + "Doe"));
+// result should be true
