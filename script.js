c7519d9197953694afde438e30ce703d2c55c58b
Franziska Kiel <kiel.franziska@outlook.com>
2025-01-15 13:23:03 +0100

    adds coding task 5

--- script.js
+++ script.js
@@ -105,3 +105,88 @@ console.log(isString(""));
 
 console.log(isString("John" + "Doe"));
 // result should be true
+
+// --------------------------------------------------------------
+// Coding Task 4
+// --------------------------------------------------------------
+// Copy the following complete code block to your script.js file
+// and try to spot and fix the errors.
+/* const size = 25;
+let result;
+if (size > 10) {
+  result = "Greater than 10";
+} else if (size > 20) {
+  result = "Greater than 20";
+} else {
+  result = "Lower than 10";
+}
+console.log(result);
+// Returns Greater than 10 but should return Greater than 20 */
+const size = 25;
+let result;
+
+if (size > 10 && size <= 20) {
+  result = "Greater than 10";
+} else if (size > 20) {
+  result = "Greater than 20";
+} else {
+  result = "Lower than 10";
+}
+
+console.log(result);
+// Returns Greater than 10 but should return Greater than 20
+
+// --------------------------------------------------------------
+// TODO: Implement a oddEven function.
+// The function has one parameter which is a number
+// If the number is an even number, the function returns the string even
+// If the number is an od number, the function returns the string odd
+function oddEven(number) {
+  if (number % 2 === 0) {
+    // The remainder (%) operator returns the remainder left over when one operand is divided by a second operand
+    return "even";
+  }
+  return "odd";
+}
+
+console.log(oddEven(4));
+// result should be even
+
+console.log(oddEven(3));
+// result should be odd
+
+console.log(oddEven(-1));
+// result should be odd
+
+console.log(oddEven(10));
+// result should be even
+
+// --------------------------------------------------------------
+// TODO: Implement an oldYoung function.
+/* The function has one parameter which is a number
+ If the number is less than 16, return "children"
+ If the number is less than 50, return "young person"
+ Otherwise return "elder person"
+ If the number is not a number or a negative number a string invalid parameter */
+
+function oldYoung(age) {
+  if (typeof age !== "number" || age < 0) {
+    return "invalid";
+  } else if (age < 16) {
+    return "child";
+  } else if (age < 50) {
+    return "young person";
+  } else return "elder person";
+}
+
+console.log(oldYoung(27));
+// result should be young person
+
+console.log(oldYoung(6));
+// result should be children
+
+console.log(oldYoung(-1));
+// result should be invalid parameter
+
+console.log(oldYoung(86));
+// result should be elder person
